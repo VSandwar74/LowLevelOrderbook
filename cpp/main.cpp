65215b02188e6595700ec9dc5feb9e28f0a62d06
Vishakh Sandwar <vishakhs74@gmail.com>
2023-08-09 16:35:02 -0400

    80% cpp update

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,91 +1,49 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <fstream>
-#include <unordered_map>
-#include <algorithm>
-#include "orderbook/orderbook.cpp" 
-#include "csv.h"
-
-struct DataFrameRow {
-    std::stringw Tick;
-    std::string Ask;
-    std::string AV;
-    std::string Bid;
-    std::string BVol;
-};
+#include "orderbook/uptoheap.cpp"
 
 int main() {
+    // Insert some elements into the heap
+    MaxHeap minHeap;
 
-    OrderBook book;
+    Order* o1 = new Order(1, 12345.6789, "Buy", 1010.0f, 10, "Client A");   
+    Order* o2 = new Order(2, 98765.4321, "Sell", 910.0f, 20, "Client B");
+    Order* o3 = new Order(3, 98765.4321, "Sell", 920.0f, 20, "Client B");
+    Order* o4 = new Order(4, 98765.4321, "Sell", 930.0f, 20, "Client B");
 
-    std::vector<std::vector<std::string>> data;
-    int ct = 0;
+    Node* n1 = new Node(o1);
+    Node* n2 = new Node(o2);
+    Node* n3 = new Node(o3);
+    Node* n6 = new Node(o4);
 
-    io::CSVReader<3> in("/Users/VishakhS/QFS/CPPOrderbook/LOBSTER_SampleFile_AMZN_2012-06-21_1/AMZN_2012-06-21_34200000_57600000_message_1.csv");
-    in.read_header(io::ignore_extra_column, 'time', 'type', 'orderid', 'size', 'price', 'direction');
-    double time; int type; int orderid, int size, float price, int direction;
-    while(in.read_row(time, type, orderid, size, price, direction)){
+    DoublyLinkedList* node1 = new DoublyLinkedList(n1);
+    DoublyLinkedList* node2 = new DoublyLinkedList(n2);
+    DoublyLinkedList* node3 = new DoublyLinkedList(n3);
+    DoublyLinkedList* node4 = new DoublyLinkedList(n6);
 
-        if (type == 1) {
-            // Place order
-            Order curr(orderid, time, (direction == 1 ? "BID" : "ASK"), price, size, "market");
-            book.placeOrder(curr);
-        } else if (type == 2) {
-            // Partial cancel
-            book.partialCancel(row.OrderId, row.Size);
-        } else if (type == 3) {
-            // Full cancel
-            book.cancelOrder(row.OrderId);
-        } else if (type == 4) {
-            // Execution of visible
-            book.executeOrder(row.OrderId);
-        } else if (type == 5) {
-            // Execution of hidden
-            continue;
-        } else if (type == 7) {
-            // Halts
-            continue;
-        }
+    minHeap.insert(node1);
+    minHeap.insert(node2);
+    minHeap.insert(node3);
+    minHeap.insert(node4);
 
-        // Retrieve bestAsk and bestBid from the OrderBook
-        std::string bestAsk = book.bestAsk.getMin();
-        std::string bestBid = book.bestBid.getMin();
+    cout << "Min element: " << minHeap.getMin() << endl;
+    cout << "Heap elements: ";
+    minHeap.printHeap();  // The elements may not be in sorted order after insertion
 
-        std::vector<std::string> bookListing;
+    // Remove the minimum element
+    minHeap.popOrder();
+    cout << "After popping the minimum element: ";
+    minHeap.printHeap();  // The heap will be restructured after the pop
 
-        if (bestAsk != "" && bestBid != "") {
-            bookListing.push_back(row.Time);
+    // Remove a specific element
+    minHeap.remove(node2);
+    cout << "After removing node2: ";
+    minHeap.printHeap();
 
-            bookListing.push_back(std::to_string(bestAsk.price));
-            bookListing.push_back(std::to_string(book.getVolumeAtPrice(bestAsk.price, "ASK")));
-
-            bookListing.push_back(std::to_string(bestBid.price));
-            bookListing.push_back(std::to_string(book.getVolumeAtPrice(bestBid.price, "BID")));
-
-            data.push_back(bookListing);
-        }
-    }
-
-    std::vector<DataFrameRow> dataList = data;
-    
-    std::ofstream outputFile("reconstructed_book.csv");
-    if (!outputFile) {
-        std::cerr << "Error opening output file." << std::endl;
-        return 1;
-    }
-
-    outputFile << "Tick,Ask,AV,Bid,BVol" << std::endl;
-
-    for (const auto& row : dataList) {
-        outputFile << row.Tick << ","
-                   << row.Ask << ","
-                   << row.AV << ","
-                   << row.Bid << ","
-                   << row.BVol << std::endl;
-    }
-
-    outputFile.close();
+    // Clean up memory
+    // delete node1;
+    // delete node2;
+    // delete node3;
+    // delete node4;
 
     return 0;
 }
